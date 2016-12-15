0d758fbd25f61e2f7cd17f03e2b91bfaee5232cc
御坂美琴 <autyan@outlook.com>
2016-12-15 23:38:43 +0800

    添加协议解码

---
+++ NatioanalEnvirmentEncoder/NatioanalEnvirmentEncoder.cs
@@ -0,0 +1,18 @@
+﻿using SHWDTech.Platform.ProtocolService;
+using SHWDTech.Platform.ProtocolService.ProtocolEncoding;
+
+namespace NatioanalEnvirmentEncoder
+{
+    public class NatioanalEnvirmentEncoder : IProtocolEncoder
+    {
+        public IProtocolPackage Decode(byte[] bufferBytes)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delive(IProtocolPackage package, IActiveClient client)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

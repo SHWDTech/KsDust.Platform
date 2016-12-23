9639d97ef8c7ab3bfc7c9d7e002e6968419fcadb
DESKTOP-KNQFNF6\AlexZhang <autyan@hotmail.com>
2016-12-23 15:48:51 +0800

    添加搜索接口

---
+++ Dust.Platform.Service/Controllers/SearchController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Dust.Platform.Service.Models;
+using Dust.Platform.Storage.Model;
+using Dust.Platform.Storage.Repository;
+
+namespace Dust.Platform.Service.Controllers
+{
+    [RoutePrefix("api/Search")]
+    public class SearchController : ApiController
+    {
+        private readonly KsDustDbContext _ctx;
+
+        public SearchController()
+        {
+            _ctx = new KsDustDbContext();
+        }
+
+        public HttpResponseMessage Get([FromUri] string searchStr)
+        {
+            var resultList = new List<SearchViewModel>();
+            resultList.AddRange(_ctx.KsDustDevices.Where(dev => dev.Name.Contains(searchStr)).Select(item => item.Name).ToList()
+                .Select(obj => new SearchViewModel { name = obj, category = AverageCategory.Device }));
+            resultList.AddRange(_ctx.KsDustProjects.Where(prj => prj.Name.Contains(searchStr)).Select(item => item.Name).ToList()
+                .Select(obj => new SearchViewModel { name = obj, category = AverageCategory.Project }));
+            resultList.AddRange(_ctx.Enterprises.Where(ent => ent.Name.Contains(searchStr)).Select(item => item.Name).ToList()
+                .Select(obj => new SearchViewModel { name = obj, category = AverageCategory.Enterprise }));
+            resultList.AddRange(_ctx.Districts.Where(dis => dis.Name.Contains(searchStr)).Select(item => item.Name).ToList()
+                .Select(obj => new SearchViewModel { name = obj, category = AverageCategory.District }));
+
+            return Request.CreateResponse(HttpStatusCode.OK, resultList);
+        }
+    }
+}
---
+++ Dust.Platform.Service/Models/SearchViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Dust.Platform.Storage.Model;
+// ReSharper disable InconsistentNaming
+
+namespace Dust.Platform.Service.Models
+{
+    public class SearchViewModel
+    {
+        public string name { get; set; }
+
+        public AverageCategory category { get; set; }
+    }
+}

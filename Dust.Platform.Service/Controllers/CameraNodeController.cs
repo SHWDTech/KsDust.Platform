87fcbee09ab8744114aa29fb3e0f45abd7ab8a9f
DESKTOP-KNQFNF6\AlexZhang <autyan@hotmail.com>
2017-02-08 10:05:14 +0800

    添加摄像头更新列表接口。

--- Dust.Platform.Service/Controllers/CameraNodeController.cs
+++ Dust.Platform.Service/Controllers/CameraNodeController.cs
@@ -1,5 +1,10 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
+using Dust.Platform.Service.Models;
 using Dust.Platform.Storage.Repository;
 
 namespace Dust.Platform.Service.Controllers
@@ -16,8 +21,59 @@ namespace Dust.Platform.Service.Controllers
 
         public HttpResponseMessage Get()
         {
+            var cameraNodes = new List<CameraNode>();
+            foreach (var district in _ctx.Districts.Where(obj => obj.Id != Guid.Empty).ToList())
+            {
+                var disNodes = new CameraNode
+                {
+                    Id = district.Id,
+                    Name = district.Name
+                };
+                var devices = _ctx.KsDustDevices.Include("Project")
+                    .Include("Project.District")
+                    .Include("Project.Enterprise")
+                    .Where(dev => dev.Project.DistrictId == district.Id && dev.Project.Id != Guid.Empty && !dev.Project.Stopped).ToList();
+                var ents = devices.Select(dev => dev.Project.Enterprise).Distinct().ToList();
+                if (ents.Any())
+                {
+                    disNodes.Children = new List<CameraNode>();
+                }
+                foreach (var ent in ents)
+                {
+                    var entNode = new CameraNode
+                    {
+                        Id = ent.Id,
+                        Name = ent.Name,
+                        Children = new List<CameraNode>()
+                    };
+                    foreach (var prj in devices.Where(obj => obj.Project.EnterpriseId == ent.Id).Select(dev => dev.Project).Distinct().ToList())
+                    {
+                        var prjNode = new CameraNode
+                        {
+                            Id = prj.Id,
+                            Name = $"{prj.Name}({prj.ContractRecord})",
+                            Children = new List<CameraNode>()
+                        };
 
-            return null;
+                        foreach (var dev in devices.Where(obj => obj.ProjectId == prj.Id).ToList())
+                        {
+                            prjNode.Children.Add(new CameraNode
+                            {
+                                Id = dev.Id,
+                                Name = dev.Name
+                            });
+                        }
+
+                        entNode.Children.Add(prjNode);
+                    }
+
+                    disNodes.Children.Add(entNode);
+                }
+
+                cameraNodes.Add(disNodes);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, cameraNodes);
         }
     }
 }
---
+++ Dust.Platform.Service/Models/CameraNode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Dust.Platform.Storage.Model;
+
+namespace Dust.Platform.Service.Models
+{
+    public class CameraNode
+    {
+        private List<CameraLogin> _cameraLogins;
+
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<CameraNode> Children { get; set; }
+
+        public List<CameraLogin> GetCamreaLogins()
+        {
+            if (_cameraLogins != null) return _cameraLogins;
+            _cameraLogins = new List<CameraLogin>();
+            if (Children == null || Children.Count <= 0) return _cameraLogins;
+            foreach (var child in Children)
+            {
+                child.LoadCameraLogins(_cameraLogins);
+            }
+
+            return _cameraLogins;
+        }
+
+        public void LoadCameraLogins(List<CameraLogin> cameraLogins)
+        {
+            cameraLogins.AddRange(GetCamreaLogins());
+        }
+    }
+}

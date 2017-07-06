862ac3ca4deab1cc27497e9313fd1519719048aa
御坂美琴auautyan <autyan@outlook.com>
2017-07-06 16:36:42 +0800

    添加安卓项目，添加按照合同备案号搜索。

--- Dust.Platform.Service/Controllers/SearchController.cs
+++ Dust.Platform.Service/Controllers/SearchController.cs
@@ -30,8 +30,46 @@ namespace Dust.Platform.Service.Controllers
                 .Select(obj => new SearchViewModel { objectId = obj.Id, objectName = obj.Name, objectLevel = AverageCategory.Enterprise }));
             resultList.AddRange(_ctx.Districts.Where(dis => dis.Name.Contains(post.searchName)).Select(item => new { item.Id, item.Name }).ToList()
                 .Select(obj => new SearchViewModel { objectId = obj.Id, objectName = obj.Name, objectLevel = AverageCategory.District }));
+            var projects = _ctx.KsDustProjects.Include(nameof(Enterprise))
+                .Include(nameof(District))
+                .Where(prj => prj.ContractRecord.Contains(post.searchName))
+                .ToList();
+            foreach (var project in projects)
+            {
+                if (resultList.All(r => r.objectId != project.EnterpriseId))
+                {
+                    resultList.Add(new SearchViewModel
+                    {
+                        objectId = project.EnterpriseId,
+                        objectLevel = AverageCategory.Enterprise,
+                        objectName = project.Enterprise.Name
+                    });
+                }
+                if (resultList.All(r => r.objectId != project.DistrictId))
+                {
+                    resultList.Add(new SearchViewModel
+                    {
+                        objectId = project.DistrictId,
+                        objectLevel = AverageCategory.District,
+                        objectName = project.District.Name
+                    });
+                }
+                var devs = _ctx.KsDustDevices.Where(d => d.ProjectId == project.Id);
+                foreach (var dev in devs)
+                {
+                    if (resultList.All(r => r.objectId != dev.Id))
+                    {
+                        resultList.Add(new SearchViewModel
+                        {
+                            objectId = dev.Id,
+                            objectLevel = AverageCategory.Device,
+                            objectName = dev.Name
+                        });
+                    }
+                }
+            }
 
-            return Request.CreateResponse(HttpStatusCode.OK, resultList);
+            return Request.CreateResponse(HttpStatusCode.OK, resultList.Distinct());
         }
     }
 }
---
+++ Ks.Dust.Android.DataPresenter/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+
+namespace Ks.Dust.Android.DataPresenter
+{
+    [Activity(Label = "Ks.Dust.Android.DataPresenter", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            var btn = FindViewById(Resource.Id.btnHello);
+            btn.Click += (sender, args) =>
+            {
+                Toast.MakeText(this, "Hello World!", ToastLength.Short).Show();
+            };
+        }
+    }
+}
+
